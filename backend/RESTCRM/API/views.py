43ccfecf4632f582f7d4a171606f5e8ca0b71be2
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-15 00:56:17 +0500

    model_to_dict

--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -2,11 +2,14 @@
 import json
 
 from django.http import JsonResponse
+from Lists.models import List
+from django.forms.models import model_to_dict
 
 
 def api_home(request, *args, **kwargs):
     '''Func Takes in Request Sends Back Request Parameters'''
-    body = request.body
-    data= json.loads(body)
-    print(data)
+    model_data = List.objects.all().order_by('?').first()
+    data= {}
+    if model_data:
+        data = model_to_dict(model_data)
     return JsonResponse(data)
