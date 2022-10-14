7ec3bd7f4c83558df3210bc8ab07e3898ac9309e
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-14 20:28:38 +0500

    Define The Task Componenet

--- backend/RESTCRM/Lists/models.py
+++ backend/RESTCRM/Lists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    '''Model Responsible for Todo List'''
+    task = models.CharField(max_length=200) 
+    is_completed = models.BooleanField(default=False)
+    
+    def __str__(self): 
+        return self.task + ' | ' + str(self.is_completed)
