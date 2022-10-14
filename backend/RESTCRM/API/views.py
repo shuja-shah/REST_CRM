babd44e4722e9b981bde09c934254077a226bf38
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-14 17:02:55 +0500

    ECHO DATA BACK TO CLIENT

--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -1,6 +1,10 @@
+import json
 from django.http import JsonResponse
 
 
 
-def api_home(request):
-    return JsonResponse({"message": "OK"})
+def api_home(request, *args, **kwargs):
+    body = request.body
+    data= json.loads(body)
+    print(data)
+    return JsonResponse(data)
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -2,7 +2,7 @@ import requests
 
 ENDPOINT= 'http://localhost:8000/api/'
 
-GET_RESPONE= requests.get(ENDPOINT, json={"message": "OK"})
+GET_RESPONE= requests.get(ENDPOINT, json={"name": "shuja"})
 
 print(GET_RESPONE.text)
 print(GET_RESPONE.json)
