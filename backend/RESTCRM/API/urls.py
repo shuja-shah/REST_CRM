77473d1735ebd253d71702d17d84aedc7bf014a6
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-14 14:34:36 +0500

    Install API URLS

---
+++ backend/RESTCRM/API/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+url_patterns = [
+    path('', views.api_home)
+]
--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def api_home(request, *args, **kwargs):
+     return JsonResponse({"message": "Hello, world!"}, status=200);
+  
