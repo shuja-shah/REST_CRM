c930a32a021cf1aa44be0b8683d5c9951fa64657
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-14 20:17:26 +0500

    Fix Errors

--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -1,9 +1,11 @@
+'''Module Responsible for API View'''
 import json
+
 from django.http import JsonResponse
 
 
-
 def api_home(request, *args, **kwargs):
+    '''Func Takes in Request Sends Back Request Parameters'''
     body = request.body
     data= json.loads(body)
     print(data)
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -2,7 +2,7 @@ import requests
 
 ENDPOINT= 'http://localhost:8000/api/'
 
-GET_RESPONE= requests.get(ENDPOINT, json={"name": "shuja"})
+GET_RESPONE= requests.get(ENDPOINT, json={"name": "shuja", "age": 23, "status": "single",})
 
 print(GET_RESPONE.text)
 print(GET_RESPONE.json)
