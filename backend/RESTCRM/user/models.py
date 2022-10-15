8bde9b845e053162bd67a1eb2e4bae1fa620fd05
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-15 21:21:40 +0500

    Define user

--- backend/RESTCRM/user/models.py
+++ backend/RESTCRM/user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from Lists.models import List
 
-# Create your models here.
+
+class user(AbstractUser):
+    
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    list = models.OneToOneField(List, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return self.username
+    
