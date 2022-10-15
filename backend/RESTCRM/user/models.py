ea731029830a4b266bd70ff7d51e91620a44998c
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-15 21:32:34 +0500

    redefine objects

--- backend/RESTCRM/user/models.py
+++ backend/RESTCRM/user/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from Lists.models import List
 
+from .user_manager import UserManager
+
 
 class user(AbstractUser):
     
@@ -9,6 +11,9 @@ class user(AbstractUser):
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     list = models.OneToOneField(List, on_delete=models.CASCADE, null=True, blank=True)
+    
+    objects = UserManager()
+    
     def __str__(self):
         return self.username
     
