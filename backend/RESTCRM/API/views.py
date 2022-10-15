02b635ca0b428750e303b5cff7b389b2b284f61a
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-16 01:05:10 +0500

    Successfully Migrate

--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -1,7 +1,7 @@
 '''Module Responsible for API View'''
 
-from Lists.models import List
-from Lists.serializers import ModelList
+# from Lists.models import List
+# from Lists.serializers import ModelList
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,16 +9,17 @@ from rest_framework.response import Response
 @api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
     '''Func Retuns Data from DB'''
-    if request.method == "GET":
-        model_instance = List.objects.all().order_by('?').first()
-        data= {}
-        if model_instance:
-            data = ModelList(model_instance).data
-        str('Not FOUND ERROR 404')
-        return Response(data)
-    if request.method == "POST":
-        serializer= ModelList(data = request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+    # if request.method == "GET":
+    #     model_instance = List.objects.all().order_by('?').first()
+    #     data= {}
+    #     if model_instance:
+    #         data = ModelList(model_instance).data
+    #     str('Not FOUND ERROR 404')
+    #     return Response(data)
+    # if request.method == "POST":
+    #     serializer= ModelList(data = request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors)
+    return(str('DEBUGIGNg'))
--- backend/RESTCRM/user/models.py
+++ backend/RESTCRM/user/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from Lists.models import List
 
 from .user_manager import UserManager
 
