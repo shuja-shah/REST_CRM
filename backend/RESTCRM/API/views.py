606a220602f7df4b39e74e224a60e1c3e7bb3223
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-15 18:52:44 +0500

    Develop POST method

--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -9,9 +9,16 @@ from rest_framework.response import Response
 @api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
     '''Func Retuns Data from DB'''
-    model_instance = List.objects.all().order_by('?').first()
-    data= {}
-    if model_instance:
-        data = ModelList(model_instance).data
-    str('Not FOUND ERROR 404')
-    return Response(data)
+    if request.method == "GET":
+        model_instance = List.objects.all().order_by('?').first()
+        data= {}
+        if model_instance:
+            data = ModelList(model_instance).data
+        str('Not FOUND ERROR 404')
+        return Response(data)
+    if request.method == "POST":
+        serializer= ModelList(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -2,7 +2,7 @@ import requests
 
 ENDPOINT= 'http://localhost:8000/api/'
 
-GET_RESPONE= requests.get(ENDPOINT, json={"id": "0", })
+GET_RESPONE= requests.post(ENDPOINT, json={"task": "Finalize The POST method", })
 
 print(GET_RESPONE.status_code)
 print(GET_RESPONE.json)
