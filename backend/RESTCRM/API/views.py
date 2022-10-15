c0561108b9706c325c416f4193afba8df1fc7499
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-15 17:22:40 +0500

    Seralize and Return API VIEW

--- backend/RESTCRM/API/views.py
+++ backend/RESTCRM/API/views.py
@@ -1,7 +1,5 @@
 '''Module Responsible for API View'''
-import json
 
-from django.forms.models import model_to_dict
 from Lists.models import List
 from Lists.serializers import ModelList
 from rest_framework.decorators import api_view
@@ -10,11 +8,10 @@ from rest_framework.response import Response
 
 @api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
-    '''Func Takes in Request Sends Back Request Parameters'''
-    qurey= request.GET.get('id')
-    model_data = List.objects.all().order_by('?').first()
+    '''Func Retuns Data from DB'''
+    model_instance = List.objects.all().order_by('?').first()
     data= {}
-    if model_data:
-        data = model_to_dict(model_data)
-        print(List.object.all())
+    if model_instance:
+        data = ModelList(model_instance).data
+    str('Not FOUND ERROR 404')
     return Response(data)
